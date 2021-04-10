73ed55ba8106fc7ff28975b7ce68bedab20d12b5
Suleiman <suleimancode@gmail.com>
2021-04-10 17:21:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+thisIsaVariable = "Hello! I am a value stored in a Variable!"
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if str == '_main_':
+    print('What is your name?') # ask for their name
+    personName = input()
+    print('It is good to meet you, ' + personName)
+
+    print('What is your age?')  # ask for their age
+    personAge = input()
+    print('You will be ' + str(int(personAge) + 1) + ' in a year.')
+
+    print('Type something and I will count the length of that sentence?')  # ask for their age
+    personAge = input()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
